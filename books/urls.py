ac2920d34033358cf432ac943368bcc203ceccf3
rakshakrawat <63923015+rakshakrawat@users.noreply.github.com>
2020-05-26 12:50:28 +0530

    Initial commit

---
+++ books/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+
+    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('books/add/', views.BookCreate.as_view(), name='book-add'),
+]
